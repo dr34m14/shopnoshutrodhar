23583de11984c8ec2adb96bd22862733e0dbf12c
shopno14 <shopno155214@gmail.com>
2023-06-04 09:26:51 -0400

    add ninja

--- route/route.js
+++ route/route.js
@@ -11,6 +11,7 @@ module.exports= function route(app){
 app.use('/project/love', express.static(path.join(file,'love')))
 app.use('/project/counter', express.static(path.join(file,'counter')))
 app.use('/project/color_flipper', express.static(path.join(file,'color_flipper')))
+app.use('/project/ninja', express.static(path.join(file,'ninja')))
 
 app.use('/project/kala_chasma', express.static(path.join(file,'kala_chasma')))
 }
